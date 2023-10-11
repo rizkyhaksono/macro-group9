a001878f4189519ac188b7992ef71338ab7abef6
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-10-11 22:31:54 +0700

    fix: image carousel on about page

--- src/components/image_slider.js
+++ src/components/image_slider.js
@@ -1,45 +1,17 @@
-import React, { useEffect, useRef } from "react";
-import Swiper from "swiper";
-import "swiper/swiper-bundle.css";
+import React from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 
 const ImageSlider = ({ images }) => {
-  const swiperRef = useRef(null);
-
-  useEffect(() => {
-    swiperRef.current = new Swiper(".swiper-container", {
-      loop: true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
-
-    // Handle next button click
-    document.querySelector(".swiper-button-next").addEventListener("click", () => {
-      swiperRef.current.slideNext();
-    });
-
-    // Handle previous button click
-    document.querySelector(".swiper-button-prev").addEventListener("click", () => {
-      swiperRef.current.slidePrev();
-    });
-  }, []);
-
   return (
-    <div className="container mx-auto swiper-container">
-      <div className="swiper-wrapper">
+    <div className="slide-container">
+      <Slide>
         {images.map((image, index) => (
           <div className="swiper-slide" key={index}>
             <img src={image} alt={`Slide ${index}`} className="" />
           </div>
         ))}
-      </div>
-      <div className="swiper-pagination"></div>
-      <div className="mr-24 swiper-button-next"></div>
-      <div className="ml-24 swiper-button-prev"></div>
+      </Slide>
     </div>
   );
 };
