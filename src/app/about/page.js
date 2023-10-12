c88975dce5472681760007d0671aefb4f86aa794
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-10-12 09:25:46 +0700

    fix: roles on about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -221,7 +221,7 @@ export default function About() {
               <div className="flex flex-col items-center pb-10">
                 <Image src="/illustration.png" className="w-24 h-24 mb-3 rounded-full shadow-lg" alt="Rizky" width={300} height={300}></Image>
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Johan</h5>
-                <span className="text-sm text-gray-500 dark:text-gray-400">Hacker</span>
+                <span className="text-sm text-gray-500 dark:text-gray-400">PM / Hustler</span>
                 <div className="flex mt-4 space-x-3 md:mt-6">
                   <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-blue-300">
                     Add friend
@@ -273,7 +273,7 @@ export default function About() {
               <div className="flex flex-col items-center pb-10">
                 <Image src="/illustration.png" className="w-24 h-24 mb-3 rounded-full shadow-lg" alt="Rizky" width={300} height={300}></Image>
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Risma</h5>
-                <span className="text-sm text-gray-500 dark:text-gray-400">Hacker</span>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Hipster</span>
                 <div className="flex mt-4 space-x-3 md:mt-6">
                   <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-blue-300">
                     Add friend
@@ -325,7 +325,7 @@ export default function About() {
               <div className="flex flex-col items-center pb-10">
                 <Image src="/illustration.png" className="w-24 h-24 mb-3 rounded-full shadow-lg" alt="Rizky" width={300} height={300}></Image>
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Arfi</h5>
-                <span className="text-sm text-gray-500 dark:text-gray-400">Hacker</span>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Hipster</span>
                 <div className="flex mt-4 space-x-3 md:mt-6">
                   <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-blue-300">
                     Add friend
