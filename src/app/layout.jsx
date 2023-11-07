603b1e82ab82c1c60de1ece8dec15819f9f65e09
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-11-07 15:30:03 +0700

    feat(analytic): add vercel analytic

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/logo.png" sizes="any" />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
